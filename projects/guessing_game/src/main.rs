28237c5587e3e1d4f5494f09d3a22fe9e6b478a9
Guzmán López <guzilop@gmail.com>
2018-02-20 00:02:57 -0300

    add variables project

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,40 +1,43 @@
-extern crate rand;
+    extern crate rand;
 
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+    use std::io;
+    use std::cmp::Ordering;
+    use rand::Rng;
 
-fn main() {
+    fn main() {
 
-    println!("Guess the number!");
-    
-    let secret_number = rand::thread_rng().gen_range(1,101);
+        println!("Guess the number!");
+        
+        let secret_number = rand::thread_rng().gen_range(1,101);
+        
+        println!("The secret number is: {}", secret_number);
 
-    println!("The secret number is: {}", secret_number);
+    let mut win = false;
 
-let mut win = false;
+    while !win {
 
-while !win {
+        println!("Please input your guess");
 
-    println!("Please input your guess");
+        let mut guess = String::new(); // mutable value
 
-    let mut guess = String::new(); // mutable value
+        io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("\n Please, insert a number... \n");
+                continue;
+            },
+            };
 
-    io::stdin().read_line(&mut guess)
-    .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse() {
-    Ok(num) => num,
-    Err(_) => continue,
-};
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => {
-            win = true;
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                win = true;
+            }
         }
     }
-}
-  println!("You win!");
-}
+    println!("You win!");
+    }
