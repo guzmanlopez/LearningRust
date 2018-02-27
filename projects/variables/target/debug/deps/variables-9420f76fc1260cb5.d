bf74bcbb45537a5c3fdd8c6f240c306d27ac4dfd
Guzmán López <guzilop@gmail.com>
2018-02-27 14:25:39 -0300

    change main.rs

---
+++ projects/variables/target/debug/deps/variables-9420f76fc1260cb5.d
@@ -0,0 +1,5 @@
+/home/guzman/rust/learning/projects/variables/target/debug/deps/variables-9420f76fc1260cb5: src/main.rs
+
+/home/guzman/rust/learning/projects/variables/target/debug/deps/variables-9420f76fc1260cb5.d: src/main.rs
+
+src/main.rs:
